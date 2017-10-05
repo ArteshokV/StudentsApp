d84ad4aa1d810a76f972fbe6796ad5786545a664
Владислав Захаров <VladZakharov@MacBook-Pro-Vladislav.local>
2017-10-05 23:34:16 +0300

    Initial Commit

---
+++ StudentsApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StudentsApp
+//
+//  Created by Владислав Захаров on 05.10.17.
+//  Copyright © 2017 Владислав Захаров. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
