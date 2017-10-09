5961b0b48e1e86b1b4cda35815da3edddcf272b4
Саверский Влад (Whiskey) <admin@Mac-Admin.local>
2017-10-10 02:18:56 +0300

    Частично реализованы заполнение и сортировка

--- StudentsApp/TaskModel.swift
+++ StudentsApp/TaskModel.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class TaskModel: NSObject {
     var taskId: Int?
-    var taskDate: Int?
+    var taskDate: String?
     var taskNameShort: String?
     var taskSubject: String?
     var taskPriority: Int?
@@ -22,10 +22,69 @@ class TaskModel: NSObject {
         var returnArray: Array<TaskModel> = Array()
         
         let firstClass: TaskModel = TaskModel()
+        firstClass.taskId = 0
+        firstClass.taskDate = "15.10.17"
+        firstClass.taskNameShort = "ДЗ1"
+        firstClass.taskSubject = "Экономика"
+        firstClass.taskPriority = 1
+        firstClass.taskDescription = "Нужно сдать этот гребаный рк"
+        firstClass.taskStatus = 0
         let secondClass: TaskModel = TaskModel()
+        secondClass.taskId = 1
+        secondClass.taskDate = "17.10.17"
+        secondClass.taskNameShort = "ДЗ1"
+        secondClass.taskSubject = "ТСиСА"
+        secondClass.taskPriority = 2
+        secondClass.taskDescription = "Нужно сдать этот гребаный рк"
+        secondClass.taskStatus = 0
+        let thirdClass: TaskModel = TaskModel()
+        thirdClass.taskId = 2
+        thirdClass.taskDate = "17.10.17"
+        thirdClass.taskNameShort = "ДЗ1"
+        thirdClass.taskSubject = "ЭВМ"
+        thirdClass.taskPriority = 1
+        thirdClass.taskDescription = "Нужно сдать этот гребаный рк"
+        thirdClass.taskStatus = 0
+        let fourthClass: TaskModel = TaskModel()
+        fourthClass.taskId = 3
+        fourthClass.taskDate = "14.11.17"
+        fourthClass.taskNameShort = "Сдать лабу №1"
+        fourthClass.taskSubject = "Программирование"
+        fourthClass.taskPriority = 0
+        fourthClass.taskDescription = "Нужно сдать этот гребаный рк"
+        fourthClass.taskStatus = 0
+        let fifthClass: TaskModel = TaskModel()
+        fifthClass.taskId = 4
+        fifthClass.taskDate = "17.10.17"
+        fifthClass.taskNameShort = "ДЗ2"
+        fifthClass.taskSubject = "Экономика"
+        fifthClass.taskPriority = 2
+        fifthClass.taskDescription = "Нужно сдать еще один гребаный рк"
+        fifthClass.taskStatus = 0
+        let sixthClass: TaskModel = TaskModel()
+        sixthClass.taskId = 3
+        sixthClass.taskDate = "14.11.17"
+        sixthClass.taskNameShort = "ДЗ2"
+        sixthClass.taskSubject = "ЭВМ"
+        sixthClass.taskPriority = 0
+        sixthClass.taskDescription = "Нужно сдать этот гребаный рк"
+        sixthClass.taskStatus = 0
+        let seventhClass: TaskModel = TaskModel()
+        seventhClass.taskId = 3
+        seventhClass.taskDate = "14.09.17"
+        seventhClass.taskNameShort = "Сдать лаб №1"
+        seventhClass.taskSubject = "ЭВМ"
+        seventhClass.taskPriority = 1
+        seventhClass.taskDescription = "Нужно сдать этот гребаный рк"
+        seventhClass.taskStatus = 0
         
         returnArray.append(firstClass)
         returnArray.append(secondClass)
+        returnArray.append(thirdClass)
+        returnArray.append(fourthClass)
+        returnArray.append(fifthClass)
+        returnArray.append(sixthClass)
+        returnArray.append(seventhClass)
         return returnArray
     }
     
---
+++ StudentsApp/TaskViewController.swift
@@ -0,0 +1,9 @@
+//
+//  TaskViewController.swift
+//  StudentsApp
+//
+//  Created by Admin on 06.10.17.
+//  Copyright © 2017 Владислав Захаров. All rights reserved.
+//
+
+import Foundation
