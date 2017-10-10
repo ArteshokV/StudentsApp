14f49218be481f410f9d49f65e0127f778056487
Владислав Захаров <VladZakharov@MacBook-Pro-Vladislav.local>
2017-10-10 04:25:25 +0300

    Добавил комментарии к коду и немного оптимизировал алгоритм

--- StudentsApp/SubjectsTab/SubjectsTabBackgroundImageLayout.swift
+++ StudentsApp/SubjectsTab/SubjectsTabBackgroundImageLayout.swift
@@ -15,16 +15,7 @@ class SubjectsTabBackgroundImageLayout: UICollectionViewFlowLayout {
     override func prepare() {
         super.prepare()
         
-        //minimumLineSpacing = 8.0
-        //minimumInteritemSpacing = 20.0
-        //minimumLineSpacing = 20
-        //sectionInset = UIEdgeInsetsMake(40, 40, 40, 40)
-        
-        //let width = (UIScreen.main.bounds.width / 3) - 2 * 8.0
-        //itemSize = CGSize(width, 100)
-        
-        
-        
+        //Регистрируем объект заднего фона (Полки)
         register(SCSBCollectionReusableView.self, forDecorationViewOfKind: "sectionBackground")
     }
     
@@ -34,31 +25,18 @@ class SubjectsTabBackgroundImageLayout: UICollectionViewFlowLayout {
         var allAttributes = [UICollectionViewLayoutAttributes]()
         
         if let attributes = attributes {
-            
+            //Для всех аттрибутов
             for attr in attributes {
-                // Look for the first item in a row
-                // You can also calculate it by item (remove the second check in the if below and change the tmpWidth and frame origin
-                if (attr.representedElementCategory == UICollectionElementCategory.cell && attr.indexPath.item % 2 == 0){ //&& attr.frame.origin.x == self.sectionInset.left) {
-                    //print("LayoutAttribute")
-                    // Create decoration attributes
+                //Находим начала каждой строки (секции) = четные ячейки
+                if (attr.representedElementCategory == UICollectionElementCategory.cell && attr.indexPath.item % 2 == 0){
+                    //Нашли строку
+                    //Создаем объект заднего фона (Полки)
                     let decorationAttributes = SCSBCollectionViewLayoutAttributes(forDecorationViewOfKind: "sectionBackground", with: attr.indexPath)
                     
-                    // Set the color(s)
-                    /*
-                    if (attr.indexPath.section % 2 == 0) {
-                        decorationAttributes.color = UIColor.green.withAlphaComponent(0.5)
-                    } else {
-                        decorationAttributes.color = UIColor.blue.withAlphaComponent(0.5)
-                    }
-                     */
-                    
-                    //decorationAttributes.color = UIColor.init(patternImage: UIImage(named: "ShelfForSubjects")!)
-                    
-                    // Make the decoration view span the entire row
-                    
+                    //Вычисляем размер заднего фона (Полки)
                     let tmpWidth = self.collectionView!.contentSize.width
-                    let tmpHeight = attr.frame.height//self.itemSize.height //+ self.minimumLineSpacing + self.sectionInset.top / 2 + self.sectionInset.bottom / 2  // or attributes.frame.size.height instead of itemSize.height if dynamic or recalculated
-                    let yValue: CGFloat = attr.frame.origin.y + attr.frame.height/2 //- self.sectionInset.top
+                    let tmpHeight = attr.frame.height
+                    let yValue: CGFloat = attr.frame.origin.y + attr.frame.height/2
                     decorationAttributes.frame = CGRect(x: 0, y: yValue, width: tmpWidth, height: tmpHeight)
                     
                     // Set the zIndex to be behind the item
@@ -95,14 +73,17 @@ class SCSBCollectionReusableView : UICollectionReusableView {
     
     override func apply(_ layoutAttributes: UICollectionViewLayoutAttributes) {
         super.apply(layoutAttributes)
+        
+        //Стираем старые View, чтобы не забивать память, т.к. ячейки reusable
         for view in (self.subviews){
             view.removeFromSuperview()
         }
-        //let scLayoutAttributes = layoutAttributes as! SCSBCollectionViewLayoutAttributes
+        
+        //Создаем объект картинки заднего фона на весь размер View
         let backgroundImage = UIImageView(frame: self.bounds)
         backgroundImage.image = UIImage(named: "ShelfForSubjects")
-        //self.view.insertSubview(backgroundImage, atIndex: 0)
-        self.addSubview(backgroundImage) //= scLayoutAttributes.color
+        //Добавляем картинку на View
+        self.addSubview(backgroundImage)
         
     }
 
--- StudentsApp/SubjectsTab/SubjectsTabViewController.swift
+++ StudentsApp/SubjectsTab/SubjectsTabViewController.swift
@@ -10,25 +10,22 @@ import UIKit
 
 class SubjectsTabViewController: UIViewController {
     
-    @IBOutlet weak var SubjectTabCollectionView: UICollectionView!
+    var subjectsArray: [SubjectModel]! //Добавляем пустой массив предметов
     
-    //Количество элементов в строке
-    let itemsPerRow = 2
+    @IBOutlet weak var SubjectTabCollectionView: UICollectionView! //Ссылка на Collection View
     
-    var subjectsArray: [SubjectModel]!
-    //Отступы от краев для каждой секции (Всего одна в данном проекте)
-    let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
+    //Настройка параметров CollectionView
+    let itemsPerRow = 2 //Количество элементов в строке
+    let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0) //Отступы от краев для каждой секции (Всего одна в данном проекте)
+    let reuseIdentifier = "subjectCollectionCell" //Идентификатор ячейки
     
-    let reuseIdentifier = "subjectCollectionCell" //
-    var items = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48"]
-    
-
-    //func colle
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        //Полуение массива предметов
         subjectsArray = SubjectModel.getSubjects()
         
+        //Добавление кастомной ячейки в Collection View
         self.SubjectTabCollectionView.register(UINib(nibName: "SubjectTabCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "subjectCollectionCell")
     }
 
@@ -42,32 +39,27 @@ class SubjectsTabViewController: UIViewController {
 // MARK: - UICollectionViewDataSource protocol
 extension SubjectsTabViewController: UICollectionViewDataSource{
     func numberOfSections(in collectionView: UICollectionView) -> Int {
-        if(subjectsArray.count % 2 == 0){
-            return subjectsArray.count/2 + 1
-        }else{
-            return subjectsArray.count/2;
-        }
-    }
-    // tell the collection view how many cells to make
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        if((section+1) * 2 > subjectsArray.count){
-            return 1
-        }
-        return 2//self.items.count
+        //Если все ячейки влезают в строки - делаем еще одну для размещения кнопки добавления, если нет, то нужна доп строка для предмета. В любом случае плюс 1
+        return subjectsArray.count/2 + 1
+       
     }
     
-    // make a cell for each cell index path
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        //Если количество выведенных ячеек сейчас будет больше, чем нам надо вывести(считая кнопку добавить), то тогда одна ячейка в строке
+        if((section+1) * 2 > subjectsArray.count+1){
+            return 1
+        }
+        return 2
+    }
+    
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        // get a reference to our storyboard cell
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath as IndexPath) as! SubjectTabCollectionViewCell
-        //let cell = Bundle.main.loadNibNamed("SubjectTabCollectionViewCell", owner: self, options: nil)?.first as! SubjectTabCollectionViewCell
-        
-        
-        // Use the outlet in our custom class to get a reference to the UILabel in the cell
-        //cell.myLabel.text = self.items[indexPath.item]
+
+        //Вычисляем индекс элемента в массиве предметов
         let index = indexPath.section * 2 + indexPath.item
         
+        //Если индекс входит в массив, то выводим предмет, если нет - то кнопку добавить
         if(index >= subjectsArray.count){
             cell.nameLabelOutlet.text = "ADD"
             cell.backgroundColor = UIColor.green
@@ -75,15 +67,15 @@ extension SubjectsTabViewController: UICollectionViewDataSource{
             cell.nameLabelOutlet.text =  subjectsArray[index].subjectName //self.items[indexPath.item]
             cell.backgroundColor = UIColor.cyan // make cell more visible in our example project
         }
-
         
         return cell
     }
 }
+
 // MARK: - UICollectionViewDelegate protocol
 extension SubjectsTabViewController: UICollectionViewDelegate{
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        // handle tap events
+        //Обработка нажатия
         print("You selected cell #\(indexPath.item)!")
     }
 }
@@ -94,12 +86,11 @@ extension SubjectsTabViewController: UICollectionViewDelegateFlowLayout{
                         layout collectionViewLayout: UICollectionViewLayout,
                         sizeForItemAt indexPath: IndexPath) -> CGSize {
         
+        //Вычисляем размер ячейки
         let paddingSpace = sectionInsets.left * CGFloat(itemsPerRow + 1)
         let availableWidth = view.frame.width - paddingSpace
         let widthPerItem = availableWidth / CGFloat(itemsPerRow)
  
-        
-        //print("HELLO \(sectionInsets.left)")
         return CGSize(width: widthPerItem, height: widthPerItem)
     }
     
@@ -107,7 +98,7 @@ extension SubjectsTabViewController: UICollectionViewDelegateFlowLayout{
     func collectionView(_ collectionView: UICollectionView,
                         layout collectionViewLayout: UICollectionViewLayout,
                         insetForSectionAt section: Int) -> UIEdgeInsets {
-        //print("HELLO insetForSectionAt \(sectionInsets.left)")
+        //Выдаем размер отступов от секции
         return sectionInsets
     }
     
@@ -115,8 +106,7 @@ extension SubjectsTabViewController: UICollectionViewDelegateFlowLayout{
     func collectionView(_ collectionView: UICollectionView,
                         layout collectionViewLayout: UICollectionViewLayout,
                         minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        //print("HELLO minimumLineSpacingForSectionAt\(sectionInsets.left)")
-        //UICollectionView.
+        //Выдаем минимальное расстояние  во длине
         return sectionInsets.left
     }
  
