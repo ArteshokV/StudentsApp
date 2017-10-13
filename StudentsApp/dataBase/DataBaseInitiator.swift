34c99249bfd60c0f508a3e6c1b481fd1c260b262
galkinivanMac <galkinivansergeevich@gmail.com>
2017-10-13 13:42:01 +0300

    Added CoreData database, added CoreData support in AppDelegate, created DBInitiator class.

---
+++ StudentsApp/dataBase/DataBaseInitiator.swift
@@ -0,0 +1,17 @@
+//
+//  DataBaseInitiator.swift
+//  StudentsApp
+//
+//  Created by Иван Галкин on 13.10.2017.
+//  Copyright © 2017 Владислав Захаров. All rights reserved.
+//
+
+import Foundation
+
+class DataBaseInitiator: NSObject {
+    
+    func insertInitialData() {
+        
+    }
+    
+}
