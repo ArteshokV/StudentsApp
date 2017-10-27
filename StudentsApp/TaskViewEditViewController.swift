1a4d19378159fdc2120b9e20a0faf9c394c5decf
Саверский Влад (Whiskey) <admin@Mac-Admin.local>
2017-10-27 16:41:45 +0300

    для восстановления

--- StudentsApp/TaskViewEditViewController.swift
+++ StudentsApp/TaskViewEditViewController.swift
@@ -23,6 +23,7 @@ class TaskViewEditViewController: UIViewController {
     
     @IBOutlet weak var TaskDateLabel: UILabel!
     
+    @IBOutlet weak var headerLabel: UILabel!
     
     @IBOutlet weak var TaskDescriptionLabel: UILabel!
     
@@ -34,11 +35,13 @@ class TaskViewEditViewController: UIViewController {
     
     
     var taskModelObject: TaskModel?
-    
+    var rounedView: UIView?
     
     override func viewDidLoad() {
         super.viewDidLoad()
     
+        
+        
         let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
         backgroundImage.image = UIImage(named: "BackGroundImage")
         self.view.insertSubview(backgroundImage, at: 0)
@@ -60,11 +63,29 @@ class TaskViewEditViewController: UIViewController {
         
         self.TaskShortNameLabel.text = taskModelObject?.taskNameShort
         self.TaskDescriptionLabel.text = taskModelObject?.taskDescription
+        print ("\( self.view.frame.width) ")
+        print ("\( TaskDescriptionLabel.frame.width) ")
+        print ("\( TaskDescriptionLabel.frame.height) ")
         self.TaskSubjectLabel.text = taskModelObject?.taskSubject
         self.TaskDateLabel.text = taskModelObject?.taskDate?.stringFromDate()
         self.TaskStatusLabel.text = taskModelObject?.taskStatus?.description
         self.TaskPriorityLabel.text = taskModelObject?.taskPriority?.description
         
+        TaskDescriptionLabel.backgroundColor = UIColor(red: 153/255, green: 157/255, blue: 163/255, alpha: 0.25)
+        TaskDateLabel.backgroundColor = UIColor(red: 153/255, green: 157/255, blue: 163/255, alpha: 0.25)
+        TaskShortNameLabel.backgroundColor = UIColor(red: 153/255, green: 157/255, blue: 163/255, alpha: 0.25)
+        TaskSubjectLabel.backgroundColor = UIColor(red: 153/255, green: 157/255, blue: 163/255, alpha: 0.25)
+        TaskStatusLabel.backgroundColor = UIColor(red: 153/255, green: 157/255, blue: 163/255, alpha: 0.25)
+        TaskPriorityLabel.backgroundColor = UIColor(red: 153/255, green: 157/255, blue: 163/255, alpha: 0.25)
+        TaskDescriptionLabel.layer.cornerRadius = 10
+        TaskDateLabel.layer.cornerRadius = 25
+        TaskShortNameLabel.layer.cornerRadius = 35
+        TaskSubjectLabel.layer.cornerRadius = 15
+        TaskStatusLabel.layer.cornerRadius = 5
+        TaskPriorityLabel.layer.cornerRadius = 5
+       
+        headerLabel.backgroundColor = UIColor(red: 153/255, green: 157/255, blue: 163/255, alpha: 0.25)
+        headerLabel.layer.cornerRadius = 5
         /*
         self.TaskDescriptionLabel.isEditable = false
         self.TaskShortNameLabel.isEditable = false
@@ -74,6 +95,8 @@ class TaskViewEditViewController: UIViewController {
         self.TaskPriorityLabel.isEditable = false
         */
         self.DoneButton.isHidden = true
+        
+       
     }
 
     override func didReceiveMemoryWarning() {
@@ -81,6 +104,29 @@ class TaskViewEditViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+/*    func getSubview (label:UILabel) -> UIView {
+        rounedView = UIView(frame: label.frame)
+        rounedView?.layer.borderColor = UIColor(red: 153/255, green: 157/255, blue: 163/255, alpha: 0.25).cgColor
+        rounedView?.layer.backgroundColor = UIColor(red: 153/255, green: 157/255, blue: 163/255, alpha: 0.25).cgColor
+        let roundedViewWidth = label.frame.width
+        let roundedViewHeight = label.frame.height
+        
+        let frameOfRoundedView = CGRect(x:0, y:0, width:roundedViewWidth - 15, height:roundedViewHeight)
+        
+        
+        
+        rounedView?.layer.masksToBounds = false
+        rounedView?.layer.cornerRadius = label.frame.width * 0.01
+        rounedView?.layer.borderWidth = 1.5
+            
+            //Настройка тени
+        rounedView?.layer.shadowOffset = CGSize(width:-2, height:2)
+        rounedView?.layer.shadowOpacity = 1
+            
+            //rounedView!.backgroundColor = UIColor.lightGray
+        rounedView?.frame = frameOfRoundedView
+        return rounedView!
+    } */
     /*
     // MARK: - Navigation
 
