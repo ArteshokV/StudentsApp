75b49592042e66bc98ecc60c939500104d96d5d0
galkinivanMac <galkinivansergeevich@gmail.com>
2017-10-28 14:41:57 +0300

    trying to fix DBcontroller file....

---
+++ StudentsApp/dataBase/DatabaseController.swift
@@ -0,0 +1,80 @@
+//
+//  DatabaseController.swift
+//  StudentsApp
+//
+//  Created by Владислав Захаров on 16.10.17.
+//  Copyright © 2017 Владислав Захаров. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class DatabaseController{
+    
+    private init(){
+        
+    }
+    
+    class func getContext() -> NSManagedObjectContext {
+        return DatabaseController.persistentContainer.viewContext
+    }
+    
+    static func timeToStringForm(Int: Int16) -> String {
+        let h = Int / 100
+        let m = Int % 100
+        let hs = h < 10 ? "0\(h)" : "\(h)"
+        let ms = m < 10 ? "0\(m)" : "\(m)"
+        return "\(hs):\(ms)"
+    }
+    
+    func timeToInt16Form(String: String) -> Int16 {
+        //FIXME: realize the method... maybe... i donno....
+        return 0
+    }
+    
+    // MARK: - Core Data stack
+    
+    static var persistentContainer: NSPersistentContainer = {
+        /*
+         The persistent container for the application. This implementation
+         creates and returns a container, having loaded the store for the
+         application to it. This property is optional since there are legitimate
+         error conditions that could cause the creation of the store to fail.
+         */
+        let container = NSPersistentContainer(name: "CoreDataModel")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                
+                /*
+                 Typical reasons for an error here include:
+                 * The parent directory does not exist, cannot be created, or disallows writing.
+                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
+                 * The device is out of space.
+                 * The store could not be migrated to the current model version.
+                 Check the error message to determine what the actual problem was.
+                 */
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    // MARK: - Core Data Saving support
+    
+    class func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+}
+
