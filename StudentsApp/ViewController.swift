75df662a4e091a3ccfe51e7b391441a025c2a6ec
Владислав Захаров <VladZakharov@MacBook-Pro-Vladislav.local>
2017-11-06 02:36:24 +0300

    App design changes

--- StudentsApp/ViewController.swift
+++ StudentsApp/ViewController.swift
@@ -30,13 +30,14 @@ class BaseTabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         //self.tabBar.tintColor = UIColor.green //Цвет выделения
-        self.tabBar.barTintColor = UIColor.white//UIColor(red: 1, green: 1, blue: 1, alpha: 0.6)
+        let appLook = CustomApplicationLook()
+        self.tabBar.barTintColor = appLook.tabBarColor //UIColor(red: 192/255, green: 140/255, blue: 83/255, alpha: 0.6)
         self.tabBar.barStyle = .blackOpaque
         //self.tabBar.alpha = 0.9
         //Добавление Blur effect в Tab bar
         let frost = UIVisualEffectView(effect: UIBlurEffect(style: .dark))
         frost.frame = self.tabBar.bounds
-        //self.tabBar.insertSubview(frost, at: 0)
+        self.tabBar.insertSubview(frost, at: 0)
         
         
         self.selectedIndex = defaultIndex
@@ -63,12 +64,13 @@ class CustomApplicationLook: NSObject{
     var mainTextColor: UIColor!
     var subTextColor: UIColor!
     var gradientView: GradientView!
+    var tabBarColor: UIColor!
     
     override init(){
         super.init()
-        
-        mainTextColor = UIColor.white
-        subTextColor = UIColor.lightGray
+        tabBarColor = UIColor(red: 192/255, green: 140/255, blue: 83/255, alpha: 0.6) //UIColor(red: 120/255, green: 120/255, blue: 120/255, alpha: 0.6)
+        mainTextColor = UIColor.black
+        subTextColor = UIColor.darkGray
 
         backgroundImage = UIImageView(frame: UIScreen.main.bounds)
         backgroundImage.image = UIImage(named: "BackGroundImage")
@@ -84,33 +86,50 @@ class CustomApplicationLook: NSObject{
         gradientView.secondColor = UIColor.white//UIColor(red: R/255, green: G/255, blue: B/255, alpha: 0.6)
         //backgroundImage.image = nil
         
-        backgroundImage.backgroundColor = UIColor.red
+        backgroundImage.backgroundColor = UIColor.white
         //UIColor(red: R/255, green: G/255, blue: B/255, alpha: 1)
+        /*
+        let currentFilter = CIFilter(name: "CIGaussianBlur")
+        let beginImage = CIImage(image: backgroundImage.image!)
+        currentFilter!.setValue(beginImage, forKey: kCIInputImageKey)
+        currentFilter!.setValue(50, forKey: kCIInputRadiusKey)
         
+        let cropFilter = CIFilter(name: "CICrop")
+        cropFilter!.setValue(currentFilter!.outputImage, forKey: kCIInputImageKey)
+        cropFilter!.setValue(CIVector(cgRect: beginImage!.extent), forKey: "inputRectangle")
+        
+        let context = CIContext(options: nil)
+        let output = cropFilter!.outputImage //cropFilter!.outputImage
+        let cgimg = context.createCGImage(output!, from: output!.extent)
+        let processedImage = UIImage(cgImage: cgimg!)
+        backgroundImage.image = processedImage
+         */
         
         let blurEffectBackground = UIBlurEffect(style: .dark)
         backgroundBlurView = UIVisualEffectView(effect: blurEffectBackground)
         backgroundBlurView.frame = UIScreen.main.bounds
         backgroundBlurView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        backgroundBlurView.alpha = 1
+        backgroundBlurView.alpha = 0.98
         
         underLayerColor = UIColor(red: 49/255, green: 51/255, blue: 89/255, alpha: 0.25)
         //UIColor(red: 153/255, green: 157/255, blue: 163/255, alpha: 0.25)
     }
     static func getUnderLayerColor() -> UIColor{
-        let R = CGFloat(255)//39) //CGFloat(arc4random_uniform(255))
-        let G = CGFloat(255)//43) //CGFloat(arc4random_uniform(255))
-        let B = CGFloat(255)//52) //CGFloat(arc4random_uniform(255))
+        let R = CGFloat(93)//39) //CGFloat(arc4random_uniform(255))
+        let G = CGFloat(118)//43) //CGFloat(arc4random_uniform(255))
+        let B = CGFloat(139)//52) //CGFloat(arc4random_uniform(255))
         return //UIColor(red: 49/255, green: 51/255, blue: 89/255, alpha: 0.7)
         //UIColor(red: 153/255, green: 157/255, blue: 163/255, alpha: 0.6)
-        //UIColor(red: R/255, green: G/255, blue: B/255, alpha: 0.5)
-        //UIColor(white: 0.6, alpha: 0.7)
-        UIColor.black.withAlphaComponent(0.3)
+        //UIColor(red: 192/255, green: 140/255, blue: 83/255, alpha: 0.3)
+        //UIColor(red: R/255, green: G/255, blue: B/255, alpha:   0.7)
+        UIColor(white: 1, alpha: 0.85)//0.075)
+        //UIColor.blue.withAlphaComponent(0.2)
+        //UIColor(red: 49/255, green: 51/255, blue: 89/255, alpha: 0.07)
     }
     
     func initBackground(ofView: UIView) {
-        ofView.insertSubview(backgroundImage, at: 0)
-        //ofView.insertSubview(gradientView, at: 0)
+        //ofView.insertSubview(backgroundImage, at: 0)
+        ofView.insertSubview(gradientView, at: 0)
         ofView.insertSubview(backgroundBlurView, at: 1)
     }
 }
